e5664df92b829afd737fcadbdf7d74617d324821
Richard <richardtully12@gmail.com>
2021-07-23 16:08:51 +0100

    its alive

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+require('dotenv').config()
 
 
 class Search extends Component {
     constructor(props){
         super(props);
-        this.state = {jsonObjext: '' };
+        this.state = {
+            trivia: ['trivia of all osrts of wonderful kinds that will amaze and adazzel yu and your family, even your friends tbh', 'will', 'show', 'up', 'here'],
+            triviaObject: '',
+            imageURL: '' };
     }
 
     getMovieObject = (searchText) => {
@@ -12,7 +16,7 @@ class Search extends Component {
         fetch(URL, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "831d7632b6msh11d653534c9f4bep1e6499jsn0e50582918e6",
+                "x-rapidapi-key": process.env.REACT_APP_API_KEY,
                 "x-rapidapi-host": "imdb8.p.rapidapi.com"
             }
         })
@@ -20,26 +24,56 @@ class Search extends Component {
             return response.json();
         })
         .then(json => {
-            console.log(json);
-            this.getMovieInfo(json);
+            this.setState({imageURL: json.results[0].image.url})
+            return this.getMovieCode(json);
+        })
+        .then(movieCode => {
+            return this.getMovieTrivia(movieCode);
         })
         .catch(err => {
             console.error(err);
         });
     }
 
-    getMovieInfo = (movieObject) => {
-        console.log(movieObject.results[0].id)
-
+    getMovieCode = (movieObject) => {
+        let movieCode = movieObject.results[0].id.split('/')[2]
+        return movieCode
     }
 
+    getMovieTrivia = (movieCode) => {
+        let URL = `https://imdb8.p.rapidapi.com/title/get-trivia?tconst=${movieCode}`
+        fetch(URL, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+                "x-rapidapi-host": "imdb8.p.rapidapi.com"
+            }
+        })
+        .then(response => {
+            
+            return response.json();
+        })
+        .then(json => {
+            let triviaText = []
+            json.unspoilt.slice(0,20).map(i => triviaText.push(i.text))
 
+            this.setState({trivia: triviaText})
+            this.state.trivia.map(i => console.log(i))
+        })
+        .catch(err => {
+            console.error(err);
+            });
+    }
 
     formatSearchText = (searchText) => {
-        // searchText = searchtext.split(' ')
         let result = searchText.split(' ').join('%20')
-        console.log(result)
         return result
+    }
+
+    triviaList = () => {
+        let x = this.state.trivia
+        const triviaList = x.map(trivia => <p>{trivia}</p>)
+        return(<ul>{triviaList}</ul>)
     }
 
 
@@ -47,9 +81,13 @@ class Search extends Component {
         return (
             <div>
                 <input placeholder = 'enter a movie name...' id = 'searchText' type="text"/>
-                <input type="submit" value='Get Trivia' onClick = {() => this.getMovieObject(document.getElementById('searchText').value)} />
-                {/* <input type="submit" value='Get Trivia' onClick = {this.getMovieCode} /> */}
-
+                <button onClick = {() => this.getMovieObject(document.getElementById('searchText').value)}>Get Trivia</button>
+                <div>
+                    <img src = {this.state.imageURL}></img>
+                </div>
+                <div>
+                    {this.triviaList()}
+                </div>
             </div>
         )
     }
